65acca1724df68c38dd82d0626961903ac23444a
Programmaniaks <programmaniaks@gmail.com>
2015-03-05 18:36:42 +0100

    Add versionning by Git tag. (To change to use branch name)

---
+++ todo-jug/buildSrc/src/main/groovy/fr/fteychene/gradlejug/buildsrc/version/BuildVersionExtension.groovy
@@ -0,0 +1,56 @@
+package fr.fteychene.gradlejug.buildsrc.version
+
+import org.gradle.api.GradleScriptException
+
+class BuildVersionExtension {
+
+    String releaseTagPattern = "^release-(\\d+\\.\\d+\\.\\d+)"
+    String matchGroup = "\$1"
+    String versionSplitter = "."
+    String snapShotQuantifier = "-SNAPSHOT"
+    boolean isRelease = false
+    String defaultVersion = "1.0.0"
+
+    private GitVersion gitVersion
+
+    public setGitVersion(GitVersion gitVersion) {
+        this.gitVersion = gitVersion
+    }
+
+    String getVersion() {
+        String version = null
+
+        try {
+            version = gitVersion.getBuildVersion(
+                    releaseTagPattern,
+                    matchGroup,
+                    versionSplitter,
+                    snapShotQuantifier,
+                    isRelease,
+                    defaultVersion
+            )
+
+        } catch (Exception e) {
+            throw new GradleScriptException("Cannot suss a build version for you.\n\n" +
+                    "Using:\n\n" +
+                    "releaseTagPattern: \"" + releaseTagPattern + "\"\n" +
+                    "matchGroup: \"" + matchGroup + "\"\n" +
+                    "versionSplitter: \"" + versionSplitter + "\"\n" +
+                    "snapShotQuantifier: \"" + snapShotQuantifier + "\"\n" +
+                    "isRelease: \"" + isRelease + "\"\n" +
+                    "git dir: \"" + gitVersion.gitDir + "\"\n" +
+                    "\n" +
+                    "Probable causes are:\n" +
+                    "\n" +
+                    "1. The directory is not a Git repo.\n" +
+                    "2. There are no annotated tags matching the releaseTagPattern.\n" +
+                    "3. The matchGroup and versionSplitter don't allow for the extraction of integers\n" +
+                    "\n" +
+                    "More info below\n\n"
+                    , e)
+        }
+
+        return version
+    }
+
+}
---
+++ todo-jug/buildSrc/src/main/groovy/fr/fteychene/gradlejug/buildsrc/version/BuildVersionPlugin.groovy
@@ -0,0 +1,22 @@
+package fr.fteychene.gradlejug.buildsrc.version
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class BuildVersionPlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+        project.configure(project) {
+            BuildVersionExtension extension = extensions.create("buildVersion", BuildVersionExtension)
+            extension.setGitVersion(new GitVersion(project.getRootProject().projectDir.absolutePath))
+        }
+
+        if (System.getProperty("isRelease") && "true".equals(System.getProperty("isRelease"))) {
+            project.allprojects {
+                buildVersion {
+                    isRelease = true
+                }
+            }
+        }
+    }
+}
